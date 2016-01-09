4d0ddf5754e5d0674e6ad4ac4e76c8aa4cd1a684
Gideon Gur <gideon.gur@seatid.com>
2016-01-09 12:48:59 +0200

    better line

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -2,12 +2,14 @@ require_relative 'line'
 require_relative 'character'
 
 class FileManager
+	INPUT_LINES_HEIGHT = 3
+	EMPTY_LINE_HEIGHT  = 1
 
 	def initialize(input_file_name)
 		@output_lines = []
 		file = File.readlines("./input_files/#{input_file_name}")
-		file.each_slice(4) do |input_lines|
-			@output_lines << Line.new([input_lines[0], input_lines[1], input_lines[2]])
+		file.each_slice(INPUT_LINES_HEIGHT+EMPTY_LINE_HEIGHT) do |input_lines|
+			@output_lines << Line.new(input_lines.take(INPUT_LINES_HEIGHT)) if input_lines.size >= INPUT_LINES_HEIGHT
 		end
 	end
 
